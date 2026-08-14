0cec7e936193526d8f1e9f2e8b439938825799e0
Miguel Vasquez <miguelangelvasquez@unicesar.edu.co>
2026-08-14 15:14:59 -0500

    feat: crear clase Cliente heredando a Persona

--- src/main/java/com/mycompany/biblioteca/Main.java
+++ src/main/java/com/mycompany/biblioteca/Main.java
@@ -11,7 +11,21 @@ public class Main {
 static ArrayList<Cliente>clientes =new ArrayList<>();
 static Scanner sc = new Scanner(System.in);
 
-    public static void main(String[] args) {
+    public static void crearCliente(String[] args) {
+        System.out.println("ID: ");
+        String id = sc.nextLine();
         
+        System.out.println("Nombre: ");
+        String nombre = sc.nextLine();
+        
+        System.out.println("Telefono: ");
+        String telefono = sc.nextLine();
+        
+        System.out.println("Email: ");
+        String email = sc.nextLine();
+        
+        clientes.add(new Cliente(id, nombre, telefono, email));
+        
+        System.out.println("Cliente creado correctamente.");
     }
 }
